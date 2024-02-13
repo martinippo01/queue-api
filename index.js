83ab0e5b032f76f4b0b0109177832467b35a710f
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2024-02-13 15:48:32 -0300

    users endpoints

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/queueDB', {
   useNewUrlParser: true,
@@ -48,13 +54,15 @@ const business = mongoose.model('business', new mongoose.Schema({
 )
   
 const user = mongoose.model('user', new mongoose.Schema({
-  name: String,
+  userName: String,
   id: Number
   },
   {
     collection: 'user'
   })
 )
+
+// -------------------------------------------- ENDPOINTS --------------------------------------------
 
 /****************************************
  * Business
@@ -77,6 +85,7 @@ app.post(BUSINESS_BASE_URL, async (req, res) => {
   }catch(e){
 
   }
+  res.status(501).json({message: "Working"})
 })
 
 app.get(BUSINESS_BASE_URL + '/:id', async (req, res) => {
@@ -86,6 +95,7 @@ app.get(BUSINESS_BASE_URL + '/:id', async (req, res) => {
   }catch(e){
 
   }
+  res.status(501).json({message: "Working"})
 })
 
 app.put(BUSINESS_BASE_URL + '/:id', async (req, res) => {
@@ -95,6 +105,7 @@ app.put(BUSINESS_BASE_URL + '/:id', async (req, res) => {
   }catch(e){
 
   }
+  res.status(501).json({message: "Working"})
 })
 
 app.patch(BUSINESS_BASE_URL + '/:id', async (req, res) => {
@@ -104,6 +115,7 @@ app.patch(BUSINESS_BASE_URL + '/:id', async (req, res) => {
   }catch(e){
 
   }
+  res.status(501).json({message: "Working"})
 })
 
 // Waiting lists
@@ -114,6 +126,7 @@ app.get(BUSINESS_BASE_URL + '/:id' + WAITING_LIST, async (req, res) => {
   }catch(e){
 
   }
+  res.status(501).json({message: "Working"})
 })
 
 app.put(BUSINESS_BASE_URL + '/:id' + WAITING_LIST, async (req, res) => {
@@ -122,11 +135,77 @@ app.put(BUSINESS_BASE_URL + '/:id' + WAITING_LIST, async (req, res) => {
   }catch(e){
 
   }
+  res.status(501).json({message: "Working"})
 })
 
 /****************************************
  * Users
 ****************************************/
+const USER_BASE_URL = '/users'
+
+app.get(USER_BASE_URL, async (req, res) => {
+  try{
+    const resp = await user.find()
+    res.status(200).json(resp)
+  }catch(e){
+    res.status(500).json({message: `error is ${e}`})
+  }
+})
+
+app.post(USER_BASE_URL, async (req, res) => {
+  try{
+    const {userName} = req.body
+    // Use the aggregation framework to calculate the next nro_cliente
+    const nextIdPipeline = [
+      {
+        $sort: { id: -1 }
+      },
+      {
+        $limit: 1
+      },
+      {
+        $project: {
+          _id: 0,
+          id: { $add: ["$id", 1] }
+        }
+      }
+    ];
+    let [nextId] = await user.aggregate(nextIdPipeline)
+    if (nextId === null || nextId === undefined){
+      nextId = 1;
+    }
+
+    user.insertMany({
+      id: nextId.id,
+      userName: userName
+    })
+    
+    res.status(201).json((await user.find({id: nextId.id}))[0])
+  }catch(e){
+    res.status(500).json({message: `error is ${e}`})
+  }
+})
+
+app.get(`${USER_BASE_URL}/:id`, async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    // Find the user by ID
+    const userFound = await user.findOne({ id: userId });
+
+    if (!userFound) {
+      // If user is not found, return a 404 Not Found response
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // If user is found, return the user data
+    res.status(200).json(userFound);
+  } catch (error) {
+    // If an error occurs, return a 500 Internal Server Error response
+    res.status(500).json({ message: `Error: ${error.message}` });
+  }
+});
+
 
 
 
